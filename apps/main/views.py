a6afca7e9f750e8ae1af9e57177fbc45a9f6a0f9
unknown <https://github.com/qwertyio19/21-1b-hws_month_3_repeat.git>
2025-02-27 00:16:00 +0600

    Реализовал логику todo

--- apps/main/views.py
+++ apps/main/views.py
@@ -9,3 +9,26 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['is_completed']
+    search_fields = ['title', 'description']
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Todo.objects.filter(user=self.request.user)
+        return Todo.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
