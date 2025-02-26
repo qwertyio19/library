efe4663d7a4a66e1a1adb77aac4419e3c0c303f0
unknown <https://github.com/qwertyio19/21-1b-hws_month_3_repeat.git>
2025-02-27 00:12:20 +0600

    Добавил пути

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,1 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.main.views import UserViewSet, TodoViewSet, DeleteAllTodosViewSet
 
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'todos', TodoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('delete_todos/', DeleteAllTodosViewSet.as_view({'delete': 'destroy'}), name="delete_all_todos"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
